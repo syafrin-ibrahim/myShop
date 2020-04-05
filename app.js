a1643fefc5029d53951ba2a28624ab004640001b
syafrin <syafrinibrahim12@gmail.com>
2020-04-04 19:45:43 -0700

    set up template

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const kon = require('./config/database.js')
 
-const app = express();
+const app = express(kon.database);
 
 //engine set up
 app.set('views', path.join(__dirname, 'views'));
-app.set('views engine',  'ejs');
+app.set('view engine',  'ejs');
 
 
 //set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//konkesi
+mongoose.connect('mongodb://localhost/my-shop');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', ()=>{
+    console.log('koneksi success');
+});
+
 app.get('/',(req, res)=>{
-    res.send('Hello World');
+    res.render('index', {
+        title : 'Home'
+    });
 });
 
 //set up server
